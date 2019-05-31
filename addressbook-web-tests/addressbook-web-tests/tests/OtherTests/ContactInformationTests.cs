4d5988911b1e549eb5d188b1e86ec0d643ebb816
Sergey Yusupov <sergey.yusupov@acumatica.com>
2019-05-31 10:23:50 +0300

    fix coomenr after review

--- addressbook-web-tests/addressbook-web-tests/tests/OtherTests/ContactInformationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/OtherTests/ContactInformationTests.cs
@@ -21,20 +21,5 @@ namespace WebAddressbookTests
 			Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
 			Assert.AreEqual(fromTable.AllMails, fromForm.AllMails);
 		}
-
-		[Test]
-		public void TestContactInformationFromTable()
-		{
-			ContactData fromTable = app.Contacts.GetInformationTable(2);
-			ContactData fromForm = app.Contacts.GetInformationFrom(2);
-
-			Assert.AreEqual(fromForm, fromTable);
-			Assert.AreEqual(fromForm.Address, fromTable.Address);
-
-
-
-			Assert.AreEqual(fromForm.AllPhones, fromTable.AllPhones);
-			Assert.AreEqual(fromForm.AllMails, fromTable.AllMails);
-		}
 	}
 }
